ee4a0f0b67649b92b45416a61d180e9fbb7512b9
niquewill <niquewill44@gmail.com>
2016-12-30 22:57:15 -0600

    reviewingcodeoptions

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,95 +1,89 @@
+var loses = 0;
+var wins = 0;
+			
+var play = function(userChoice) {
+					
+					document.getElementById("player").innerHTML="";
+					document.getElementById("opponent").innerHTML="";
+					document.getElementById("results").innerHTML="";
+				
+					
+					if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
+						document.getElementById("player").innerHTML='You chose' + ' ' + userChoice + '.';
+					} else if (userChoice == "rope") {
+						document.getElementById("player").innerHTML='You chose' + ' ' + userChoice + '. <br />Well aren\'t you a smarty pants.';
+					} else {
+						document.getElementById("player").innerHTML="That is not a valid choice, try again.";
+						
+						return false;
+					}
+				
+					var computerChoice = Math.random();
+					if (computerChoice < 0.34) {
+						computerChoice = "rock";
+					} else if(computerChoice <= 0.67) {
+						computerChoice = "paper";
+					} else {
+						computerChoice = "scissors";
+					}
+				 
+				 	document.getElementById("opponent").innerHTML='// Your opponent chose' + ' ' + computerChoice + '.';
+				 
+					 var compare = function (choice1,choice2) {
+						if (choice1 == choice2) {
+							return "The result is a tie!";
+						} else if (choice1 == "rock"){
+							if (choice2 =="scissors") {
+								wins++;
+								return "rock wins. rock on.";
+							} else {
+								loses++;
+								return "sorry. paper wins.";
+							}
+						} else if (choice1 == "paper") {
+							if (choice2 == "rock") {
+								wins++;
+								return "paper wins";
+							} else {
+								loses++;
+								return "sorry. scissors win.";
+							}
+						} else if (choice1 == "scissors") {
+							if (choice2 == "rock") {
+								loses++;
+								return "sorry. rock wins";
+							} else {
+								wins++;
+								return "scissors win";
+							}
+						} else if (choice1 == "rope") {
+							wins++;
+							return "rope FTW";
+						} else {
+							return "error. bummer dude. game over. no dice.";
+						}
+					};
 
-  // ON CLICK 
-// LOGIN
-// ON CLICK  
-// START
-//TWO PLAYERS APEAR
-    var userChoice = prompt("Do you choose rock, paper,  or scissors");
-    var computerChoice = Math.random();
-//    console.log(computerChoice);
-    if(computerChoice<0.33){
-        computerChoice = "rock";
-    } else if(0.34<=computerChoice<=0.66){
-        computerChoice = "paper";
-    } else {
-        computerChoice = "scissors";
-    }
- 
-    choice1 = userChoice;
-    choice2 = computerChoice;
-    function compare(choice1, choice2)
-		{
-        if(choice1===choice2){
-            return("Dang Man!!!! It's a tie");
-            }
-        if(choice1==="rock"){
-            if(choice2==="scissors"){
-                return('The Rock Wins!');
-            }else{
-                return('Paper Wins!');
-            }
-        }
-            if(choice1==="paper"){
-            if(choice2==="rock"){
-                return('The Paper Wins!');
-            }else{
-                return('Scissors Wins!');
-            }
-        }
-               if(choice1==="scissors"){
-            if(choice2==="rock"){
-                return('The Rock Wins!');
-            }else{
-                return('Scissors Wins!');
-            }
-                 
-         }  
-      }
- // VARIABLES
-    // --------------------------------------------------------------------------------
+					var winner = compare(userChoice,computerChoice);
+					document.getElementById("results").innerHTML=winner;
+					document.getElementById("wins").innerHTML=wins;
+					document.getElementById("loses").innerHTML=loses;
+					
+					if (wins > 99 || loses > 99) {
+						document.getElementById("wins").style.fontSize="44";
+						document.getElementById("loses").style.fontSize="44";
+					}
+					if (wins > 999) {
+						alert("You reached the max score of 999. <br />Congratulations, you have no life.");
+					}
+					if (loses > 999) {
+						alert("Your opponent reached the max score of 999. <br />We're sorry, you have no life.");
+					}
+			};
 
-    // Get a reference to the database service
-    var database = firebase.database();
-
-    // Setting initial value of our click counter variable to 0
-    var clickCounter = 0;
-
-    // FUNCTIONS + EVENTS
-    // --------------------------------------------------------------------------------
-
-    // On Click of Button
-    $("#click-button").on("click", function() {
-
-      // Add to clickCounter
-      clickCounter++;
-
-      //  Store Click Data to Firebase in a JSON property called clickCount
-      // Note how we are using the Firebase .set() method
-      database.ref().set({
-        clickCount: clickCounter
-      });
-    });
-
-    // MAIN PROCESS + INITIAL CODE
-    // --------------------------------------------------------------------------------
-
-    // Using .on("value", function(snapshot)) syntax will retrieve the data
-    // from the database (both initially and every time something changes)
-    // This will then store the data inside the variable "snapshot". We could rename "snapshot" to anything.
-    database.ref().on("value", function(snapshot) {
-
-      // Then we console.log the value of snapshot
-//      console.log(snapshot.val());
-
-      // Then we change the html associated with the number.
-      $("#click-value").html(snapshot.val().clickCount);
-
-      // Then update the clickCounter variable with data from the database.
-      clickCounter = snapshot.val().clickCount;
-
-    // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
-    // Again we could have named errorObject anything we wanted.
-    }, function(errorObject){
-        
-    });
-  
+var reset = function() {
+	loses = 0;
+	wins = 0;
+	document.getElementById("wins").innerHTML=wins;
+	document.getElementById("loses").innerHTML=loses;
+};
